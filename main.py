7d665f1a05c261ac2308904cf5b3dc555558eae7
Nikhil-Richhariya <richhariyan@rknec.edu>
2024-04-09 13:13:14 +0530

    added movement of player

--- main.py
+++ main.py
@@ -20,21 +20,53 @@ playerImg = pygame.image.load("Resources/player.png")
 playerX = 370 #near 400 for middle of window
 playerY = 480 #near 500 for lower side in the window
 
-def player() : 
-    screen.blit(playerImg, (playerX,playerY))
+changeInX = 0
+
+speed = 0.1 # => control speed of movement
+
+def player(x,y): 
+    screen.blit(playerImg, (x,y))
+
+def movePlayerLeft():
+    global playerX
+    playerX = ( playerX - 0.1 ) % 800
+def movePlayerRight():
+    global playerX
+    playerX = ( playerX + 0.1 ) % 800
+
+
 
 
 running = True; 
 #Game Loop 
+
 while running: 
+
+    #RGB
+    screen.fill( (0,0,0) )
     for event in pygame.event.get():
         if event.type == pygame.QUIT :
             running = False
     
-    #RGB
-    screen.fill( (0,0,0) )
+        # if keystroke is pressed check wheater its right or left
+        if event.type == pygame.KEYDOWN:
+            if (event.key == pygame.K_a or event.key == pygame.K_LEFT ):
+                changeInX = -speed #decrement the value of x  
+            elif (event.key == pygame.K_d or event.key == pygame.K_RIGHT) :
+                changeInX = speed #increment the value of Y
+        
+        #if keystroke is released 
+        if event.type == pygame.KEYUP:
+            if(event.key == pygame.K_a or event.key == pygame.K_LEFT or
+               event.key == pygame.K_d or event.key == pygame.K_RIGHT ):
+                changeInX = 0
+
+
+    #updating the value of playerX 
+    playerX += changeInX
+    playerX %= 800
 
     #should be called after fill method other wise, player would be below the 
-    player()
+    player(playerX,playerY)
 
     pygame.display.update()
